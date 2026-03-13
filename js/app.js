264b2638c485f2161b853fdb51695349649a8cec
MedniyON <rdgwcmxwz9@privaterelay.appleid.com>
2026-03-13 18:10:04 +0200

    feat: add task deletion by ID

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ const taskForm = document.querySelector(".js-task-form-wrapper");
 const taskInput = document.querySelector("[data-text-input]");
 const taskSubmitBtn = document.querySelector("[data-add-todo-btn]");
 const taskList = document.querySelector("[data-todo-container]");
+// const removeBtn = document.querySelector(".remove-btn");
 
 const taskArr = [];
 
@@ -11,7 +12,11 @@ function handlerSubmitForm(evt) {
   evt.preventDefault();
 
   if (!taskInput.value.trim()) return;
-  taskArr.push(taskInput.value);
+  taskArr.push({
+    id: Date.now(),
+    text: taskInput.value,
+    isDone: false,
+  });
   taskInput.value = "";
 
   render();
@@ -19,14 +24,26 @@ function handlerSubmitForm(evt) {
 
 function render() {
   taskList.innerHTML = "";
-  taskArr.forEach((task) => {
-    const taskItem = createElement("li", task);
+  taskArr.forEach(({ text, id }) => {
+    const taskItem = createElement("li", text, "task");
+    const removeBtn = createElement("button", "❌", "remove-btn");
+
+    removeBtn.addEventListener("click", () => removeTask(id));
+
+    taskItem.append(removeBtn);
     taskList.append(taskItem);
   });
 }
 
-function createElement(tagName, textContent) {
+function createElement(tagName, textContent, className) {
   const element = document.createElement(tagName);
   element.textContent = textContent;
+  element.classList.add(className);
   return element;
+}
+
+function removeTask(id) {
+  const index = taskArr.findIndex((task) => task.id === id);
+  taskArr.splice(index, 1);
+  render();
 }
